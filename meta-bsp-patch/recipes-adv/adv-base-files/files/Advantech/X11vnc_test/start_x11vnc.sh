90ea5a2e28d9c336210862e54948c6e3f8968ee9
james0718 <yaok0710@163.com>
2021-09-02 14:53:45 +0800

    update files adv-base-files and kernel patch

---
+++ meta-bsp-patch/recipes-adv/adv-base-files/files/Advantech/X11vnc_test/start_x11vnc.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+export DISPLAY=:0
+HISTORY=$(cd `dirname $0`; pwd)
+LOGPATH=$HISTORY/x11vnc-start.log
+
+killprocess()
+{
+	PID=`ps aux |grep "$1" |grep -v grep |grep -v start_x11vnc.sh`
+
+	if [ "x$PID" = "x" ]; then
+		echo "$1 not process!" >> $LOGPATH
+	else
+		PID=`echo $PID |awk -F ' ' '{print $2}'`
+		echo "kill $1 now" >> $LOGPATH
+		kill -9 $PID
+	fi
+}
+
+killprocess x11vnc
+
+if [ ! -d /var/run/lightdm/root/ ]; then
+	# If this is yocto filesystem.
+	CMDLINE=`cat /proc/cmdline |grep com1-485`
+	if [ "x$CMDLINE" = "x" ]; then
+		echo "Start with com1-232/dbg mode." > $LOGPATH
+	else
+		echo "Start with com1-485 mode, need to restart Xorg server." > $LOGPATH
+		killprocess matchbox-window-manager
+		killprocess matchbox-desktop
+		killprocess matchbox-panel
+		killprocess matchbox-keyboard
+		killprocess "/usr/bin/Xorg :0 -br -pn -nolisten tcp +iglx"
+		killprocess "Xorg"
+		sleep 1
+
+		Xorg &
+		matchbox-window-manager -theme Sato -use_desktop_mode decorated -use_cursor yes &
+		matchbox-desktop &
+		matchbox-panel --titlebar --start-applets showdesktop,windowselector --end-applets clock,battery,keyboard,systray,startup-notify,notify &
+		matchbox-keyboard -d &
+		sleep 1
+	fi
+fi
+
+echo "##############################################" >> $LOGPATH
+ps aux |grep Xorg |grep -v grep >> $LOGPATH
+ps aux |grep box |grep -v grep >> $LOGPATH
+echo "##############################################" >> $LOGPATH
+echo "Start x11vnc server..." >> $LOGPATH
+
+if [ ! -d /var/run/lightdm/root/ ]; then
+	# If this is yocto filesystem.
+	#x11vnc -ncache 10 -forever -shared -capslock -rfbport 5900 -rawfb console >> $LOGPATH &
+	x11vnc -ncache 10 -forever -shared -capslock -rfbport 5900 >> $LOGPATH &
+else
+	# If this is ubuntu filesystem.
+	x11vnc -ncache 10 -forever -shared -capslock -rfbport 5900 -auth /var/run/lightdm/root/:0 -display :0 >> $LOGPATH &
+fi
+
+sync
+exit
--- meta-bsp-patch/recipes-kernel/linux/linux-imx_4.19.35.bbappend
+++ meta-bsp-patch/recipes-kernel/linux/linux-imx_4.19.35.bbappend
@@ -1,12 +1,19 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 
+# file:0001-update-poweroff-problem.patch
 SRC_URI += "file://0001-config-and-dts.patch \
+            file://0001-add-cp210x-serial-driver.patch \
             file://0002-mcu-watchdog.patch \
             file://0003-fec-clock.patch \
             file://0004-rtl8211f.patch \
             file://0005-mcu-reboot.patch \
             file://0006-io-reset.patch \
             file://0001-enable-pci-controller.patch \
+            file://0001-update-rtc-rx8010-and-add-tpc71w-devicetree-ldb-date.patch \
+            file://0001-update-CONFIG-RTC-HCTOSYS-and-SYSTOHC.patch \
             file://0001-fix-mcu-reboot-unstable.patch \
+            file://0001-update-poweroff-problem.patch \
+            file://0001-update-ppp-support.patch \
+            file://0001-add-fm25-module-supported.patch \
             "
 LOCALVERSION = ""
